1ad5a2f3bf037b4a237295e1872f00d725891389
allenyw1 <43621744+allenyw1@users.noreply.github.com>
2024-09-11 15:56:13 -0700

    Optimized some functions and variables

--- pi_pico_code.cpp
+++ pi_pico_code.cpp
@@ -7,29 +7,28 @@
 
 // USER CONFIGURABLE SETTINGS
 #define ACTUATION_POINT 2 // Distance from top for actuation (in mm)
-#define RAPID_TRIGGER true // Enable/disable rapid trigger
 #define RT_RELEASE 1 // Minimum release distance to reset rapid trigger (in mm)
-
 
 // GLOBAL VARIABLES
 int actuation_level; // Level conversion of actuation distance (mm to levels)
 int current_read = 0; // Raw input reading from HE sensor
 int current_level = 0; // Current switch depression level
+bool rapid_trigger_enable = false;
 int local_max_level = 0; // Used for rapid trigger
 int actuated = 0; // 1 means actuated, 0 means not actuated
 
-int calibration_toggle = 0; // 1 for calibration mode
+bool calibration_toggle = false;
 int calibration_time = 0; // Used for checking button debounce
-int calibration_trigger = 0;
+bool calibration_trigger = false;
 int read_max;
 int read_min;
 int adc_range = 1023;
 
 
 void setup() {
-  Serial.begin(115200);
+  Serial1.begin(115200);
   actuation_level = 10 * ACTUATION_POINT;
-
+  
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), calibration_isr, FALLING);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
@@ -39,43 +38,43 @@ void loop() {
   current_level = (current_read - read_min) * LEVELS / adc_range;
 
   // Handle calibration mode toggle
-  if (calibration_trigger == 1) {
+  if (calibration_trigger) {
     if (millis() + DEBOUNCE_TIME > calibration_time) {
       calibration_time = millis(); // Debounce
 
-      if (calibration_toggle == 0) {
-        calibration_toggle = 1;
+      if (!calibration_toggle) {
+        calibration_toggle = true;
         // Reset max and min readings
         read_max = 0;
         read_min = current_read;
-      } else if (calibration_toggle == 1){
-        calibration_toggle = 0;
+      } else if (calibration_toggle){
+        calibration_toggle = false;
         adc_range = read_max - read_min; // Calculate and set new range
       }
     }
-    calibration_trigger = 0;
+    calibration_trigger = false;
   }
  
- 
-  if (calibration_toggle == 0) {
+  // Main process
+  if (!calibration_toggle) {
     // Device mode
-    if (RAPID_TRIGGER) {
+    if (rapid_trigger_enable) {
       true_rapid_trigger();
     } else {
       false_rapid_trigger();
     }
 
-    Serial.print("DEVICE:    ");
-    Serial.print(actuated);
-    Serial.print("    ");
-    Serial.println(current_level);
+    Serial1.print("DEVICE:    ");
+    Serial1.print(actuated);
+    Serial1.print("    ");
+    Serial1.println(current_level);
   } else {
     // Calibration mode
     calibrate();
-    Serial.print("CALIBR:    ");
-    Serial.print(read_min);
-    Serial.print("    ");
-    Serial.println(read_max);
+    Serial1.print("CALIBR:    ");
+    Serial1.print(read_min);
+    Serial1.print("    ");
+    Serial1.println(read_max);
   }
 
   delay(1); // this speeds up the simulation
@@ -83,20 +82,20 @@ void loop() {
 
 // ISR when button is pressed
 void calibration_isr() {
-  calibration_trigger = 1;
+  calibration_trigger = true;
 }
 
 // Find maximum and minimum sensor readings
 void calibrate() {
-  if (analogRead(ADC_PIN) > read_max) {
-    read_max = analogRead(ADC_PIN);
+  if (current_read > read_max) {
+    read_max = current_read;
   }
-  if (analogRead(ADC_PIN) < read_min) {
-    read_min = analogRead(ADC_PIN);
+  if (current_read < read_min) {
+    read_min = current_read;
   }
 }
 
-int true_rapid_trigger() {
+void true_rapid_trigger() {
   if (current_level == 0 || current_level + RT_RELEASE * 10 < local_max_level) {
     local_max_level = current_level;
     actuated = 0;
@@ -107,10 +106,6 @@ int true_rapid_trigger() {
   }
 }
 
-int false_rapid_trigger() {
-  if (current_level < actuation_level) {
-    actuated = 0;
-  } else {
-    actuated = 1;
-  }
+void false_rapid_trigger() {
+  actuated = current_level >= actuation_level;
 }
